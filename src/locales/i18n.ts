819c7a4140fd2bf37daa7b75fbe0077dffb5efaa
Uglyboy <25730357+uglyboy-tl@users.noreply.github.com>
2026-03-31 11:13:01 +0800

    docs(i18n): update translation files

--- src/locales/i18n.ts
+++ src/locales/i18n.ts
@@ -1,36 +1,33 @@
 import Mustache from "mustache";
 import { moment } from "obsidian";
 import en from "./en.json";
-import zh_cn from "./zh_cn.json";
 import pl from "./pl.json";
+import zh_cn from "./zh_cn.json";
 
 export const LANGS = {
-	en: en,
-	zh_cn: zh_cn,
-	pl: pl,
+  en: en,
+  zh_cn: zh_cn,
+  pl: pl,
 };
 
 export type LangType = keyof typeof LANGS;
 export type TransItemType = keyof (typeof LANGS)["en"];
 
 export class I18n {
-	_get(key: TransItemType) {
-		let realLang: LangType = "en";
-		if (moment.locale().replace("-", "_") in LANGS) {
-			realLang = moment.locale().replace("-", "_") as LangType;
-		}
+  _get(key: TransItemType) {
+    let realLang: LangType = "en";
+    if (moment.locale().replace("-", "_") in LANGS) {
+      realLang = moment.locale().replace("-", "_") as LangType;
+    }
 
-		const res: string =
-			(LANGS[realLang] as (typeof LANGS)["en"])[key] ||
-			LANGS["en"][key] ||
-			key;
-		return res;
-	}
+    const res: string = (LANGS[realLang] as (typeof LANGS)["en"])[key] || LANGS.en[key] || key;
+    return res;
+  }
 
-	t(key: TransItemType, vars?: Record<string, string>) {
-		if (vars === undefined) {
-			return this._get(key);
-		}
-		return Mustache.render(this._get(key), vars);
-	}
+  t(key: TransItemType, vars?: Record<string, string>) {
+    if (vars === undefined) {
+      return this._get(key);
+    }
+    return Mustache.render(this._get(key), vars);
+  }
 }
