d66edc729e58be04766888e44b646e0880ca22ac
Uglyboy <25730357+uglyboy-tl@users.noreply.github.com>
2026-03-31 11:14:08 +0800

    test(mocks): update obsidian mock for testing

--- src/__mocks__/obsidian.ts
+++ src/__mocks__/obsidian.ts
@@ -6,11 +6,21 @@ export class TFile {}
 export class TAbstractFile {}
 export class PluginSettingTab {}
 export class Setting {
-	setName() { return this; }
-	setDesc() { return this; }
-	setHeading() { return this; }
-	addSlider() { return this; }
-	addToggle() { return this; }
+  setName() {
+    return this;
+  }
+  setDesc() {
+    return this;
+  }
+  setHeading() {
+    return this;
+  }
+  addSlider() {
+    return this;
+  }
+  addToggle() {
+    return this;
+  }
 }
 export const Platform = { isMobile: false };
 export type ViewStateResult = Record<string, unknown>;
